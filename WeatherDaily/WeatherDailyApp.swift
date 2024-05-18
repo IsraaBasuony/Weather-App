8ba3ce8b260cd2c20cbd5f485cfc1f76a77d8b0e
Israa <isramuhammad613@gmail.com>
2024-05-18 11:59:02 -0700

    Initial Commit

---
+++ WeatherDaily/WeatherDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDailyApp.swift
+//  WeatherDaily
+//
+//  Created by Israa on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
